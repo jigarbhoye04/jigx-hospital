7b96fac768b4d53bcb22e086fd8020b9f3a53dd0
jigx.04 <84662430+jigarbhoye04@users.noreply.github.com>
2025-01-19 12:30:30 +0530

    added: shadcn and components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="text-white flex flex-col items-center justify-center h-screen">
       <h1 className="text-3-xl underline">HOME</h1>
+      <Button>test me!</Button>
     </div>
   );
 }
